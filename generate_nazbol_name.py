75b7a9bc0038f633eddb87f1461723fd7441f0d3
Manel <manumcraft@gmail.com>
2021-07-24 23:20:28 +0200

    init commit

---
+++ generate_nazbol_name.py
@@ -0,0 +1,55 @@
+names = [
+"stalin",
+"patriota",
+"ibero",
+"camarada",
+"rojo",
+"proletario",
+"octubre",
+"bolchevique",
+"tankie",
+"basado",
+"soldado",
+"1917",
+"obrero",
+"socialismo",
+"1953",
+"jacobino",
+"hoxhista"
+]
+flags = ["🇪🇸", "🇮🇷", "🇵🇸", "🇱🇦", "🇰🇵", "🇻🇳", "🇨🇺"]
+symbols = ["⚒️","☭", "#SPEXIT"]
+
+def generate_nazbol_name():    
+    import random
+
+    first_position_symbol = random.randint(0, 1)
+    active_symbol = random.randint(0, 1)
+    flag_number = random.randint(1, len(flags) - 1)
+    first_name = names[random.randint(0, len(names) - 1)]
+    second_name = names[random.randint(0, len(names) - 1)]
+
+    while first_name == second_name:
+        first_name = names[random.randint(0, len(names) - 1)]
+        second_name = names[random.randint(0, len(names) - 1)]
+
+    flag_string = ""
+    for i in range(flag_number):
+        flag = flags[random.randint(0, len(flags) - 1)]
+
+        if flag not in flag_string:
+            flag_string += flag
+
+    message = first_name.capitalize() + " " + second_name.capitalize() + " "
+
+    if active_symbol:
+        if first_position_symbol:
+            message += symbols[random.randint(0, len(symbols) - 1)]
+            message += flag_string
+        else:
+            message += flag_string
+            message += symbols[random.randint(0, len(symbols) - 1)]
+    else:
+        message += flag_string
+
+    return message
---
+++ main.py
@@ -0,0 +1,85 @@
+import tweepy, os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from time import sleep
+from generate_nazbol_name import generate_nazbol_name
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+
+# Chrome webdriver
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
+options.add_argument("--headless")
+options.add_experimental_option("mobileEmulation", { "deviceName": "Pixel 2" })
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+def get_last_mention_id():
+    file = open('mentions_id.txt', 'r')
+    lineList = file.readlines()
+    file.close()
+
+    if not lineList:
+        return None
+
+    id = int(lineList[-1])
+    return id
+
+def store_last_mention_id(id):
+    with open('mentions_id.txt', 'a') as file:
+        file.write(str(id) + '\n')
+        file.close()
+
+def document_initialised(driver):
+    return driver.execute_script("console.log('cargao')")
+
+def generate_image(tweet_url, name, driver):
+    # Scrap web and modify HTML
+    driver = webdriver.Chrome('C:/Users/manum/Desktop/nazbolizaBot/chromedriver.exe', options=options)
+    driver.get(tweet_url)
+    sleep(5)
+
+    try:
+        element = driver.find_element_by_xpath("//span[contains(text(), '"+ name +"')]")
+        print(element)
+        driver.execute_script("arguments[0].innerText='"+ generate_nazbol_name() +"'", element)
+    except NoSuchElementException:
+        pass
+
+    # Make capture
+    driver.get_screenshot_as_file("temp/capture.png")
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome('C:/Users/manum/Desktop/nazbolizaBot/chromedriver.exe', options=options)
+    mentions = api.mentions_timeline(get_last_mention_id(), tweet_mode = 'extended')
+
+    for mention in reversed(mentions):
+        if not '@nazbolizaBot' in mention.__dict__['full_text']:
+            store_last_mention_id(mention.id)
+            continue
+
+        tweet_url = 'https://twitter.com/{}/status/{}'.format(mention.__dict__['author'].screen_name, mention.__dict__['id'])
+        generate_image(tweet_url, mention.__dict__['user'].name, driver)
+        
+        api.update_with_media("temp/capture.png", 
+        status="",
+        in_reply_to_status_id=mention.id_str,
+        auto_populate_reply_metadata=True
+        )
+        store_last_mention_id(mention.id)
+    driver.close()
