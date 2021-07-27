a4ed6d525bdf34e6d698d4ebf53da852fae9b254
Manel <manel.mata@grandvoyage.com>
2021-07-27 10:29:40 +0200

    now makes capture of replied tweet

--- main.py
+++ main.py
@@ -10,24 +10,39 @@ from selenium.common.exceptions import NoSuchElementException
 
 from generate_nazbol_name import generate_nazbol_name
 
+
+
 class Listener(StreamListener):
     def on_data(self, tweet):
-        tweet = json.loads(tweet)
         bot_name = os.getenv('ACCOUNT_NAME')
+        replied_tweet = json.loads(get_replied_tweet(tweet))
+        tweet = json.loads(tweet)
 
-        if tweet['user']['screen_name'].lower() == bot_name: # If it is a bot interaction
-            return
-
-        if '@' + bot_name not in tweet['text'].lower(): # Has not mentioned
-            return
-
-        if tweet['in_reply_to_status_id'] and tweet['text'].lower().count('@' + bot_name) != 1: # If it is a reply
+        # Not explicit mention to bot
+        if hasattr(tweet,'display_text_range'):
+            if '@' + bot_name not in tweet.text[tweet.display_text_range[0]:].lower():
+                return
+        if hasattr(tweet,'retweeted_status'):
             return
 
         print('Procesando tuit de: '+ tweet['user']['screen_name'])
-        reply_tweet(tweet)
+        reply_tweet(replied_tweet)
         print('Procesado.')
 
+
+
+def set_up_auth():
+    auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
+    auth.set_access_token(os.getenv('ACCESS_KEY'), os.getenv('ACCESS_SECRET'))
+    api = tweepy.API(auth)
+    return api, auth
+
+
+def follow_stream():
+    api, auth = set_up_auth()
+    listener = Listener(StreamListener)
+    stream = Stream(auth, listener)
+    stream.filter(track=[os.getenv('ACCOUNT_NAME')])
 
 
 def reply_tweet(tweet):
@@ -44,18 +59,15 @@ def reply_tweet(tweet):
                             auto_populate_reply_metadata=True
                             )
 
-def set_up_auth():
-    auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
-    auth.set_access_token(os.getenv('ACCESS_KEY'), os.getenv('ACCESS_SECRET'))
-    api = tweepy.API(auth)
-    return api, auth
 
-
-def follow_stream():
+def get_replied_tweet(tweet):
     api, auth = set_up_auth()
-    listener = Listener(StreamListener)
-    stream = Stream(auth, listener)
-    stream.filter(track=[os.getenv('ACCOUNT_NAME')])
+    try:
+        replied_tweet = api.get_status(tweet.in_reply_to_status_id)
+    except Exception as e:
+        print(e)
+        return
+    return replied_tweet
 
 
 def generate_image(tweet_url, name):
@@ -63,10 +75,13 @@ def generate_image(tweet_url, name):
     options = webdriver.FirefoxOptions()
     options.add_argument('--disable-extensions')
     options.add_argument("--headless")
-    executable_path = os.path.join(os.path.dirname(__file__), 'drivers/geckodriver')
+    
     user_agent = 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36'
+    executable_path = os.path.join(os.path.dirname(__file__), 'drivers/geckodriver')
+    
     profile = webdriver.FirefoxProfile()
     profile.set_preference('general.useragent.override', user_agent)
+    
     
     # Init driver
     driver = webdriver.Firefox(executable_path=executable_path, options=options, firefox_profile=profile, service_log_path=os.devnull)
