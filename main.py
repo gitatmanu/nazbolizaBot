30c32cbbd7bc95237ed9dbf3869aca50c0350d6f
Manel <manumcraft@gmail.com>
2021-08-01 06:30:47 +0200

    a

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def generate_image(tweet_url, name):
 
 def init_driver():
     user_agent = 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36'
-    executable_path = os.path.join(os.path.dirname(__file__), 'drivers/geckodriver.exe')
+    executable_path = os.path.join(os.path.dirname(__file__), 'drivers/geckodriver')
     
     # Firefox webdriver
     options = webdriver.FirefoxOptions()
